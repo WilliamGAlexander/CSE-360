6dc29bae270232615566f421fd6bcef72565a64d
William Alexander <wgalexan@asu.edu>
2019-02-24 16:30:55 -0700

    First commit for assignment 2.

---
+++ CSE 360 Assignment 2/src/cse360assign2/Calculator.java
@@ -0,0 +1,53 @@
+package cse360assign2;
+/*
+ * William Alexander
+ * Class ID: 310
+ * Assignment 2
+ */
+
+
+/**
+ * The Calculator class performs addition, subtraction, multiplication,
+ * and division. There is a method to return the current value and another
+ * method that prints the history of previous calculations
+ * @author William Alexander
+ * @version 1.0
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	public int getTotal () {
+		return 0;
+	}
+
+	public void add (int value) {
+
+	}
+
+	public void subtract (int value) {
+
+		
+	}
+
+	public void multiply (int value) {
+
+		
+	}
+
+	public void divide (int value) {
+
+		
+	}
+
+	public String getHistory () {
+		
+	
+		return "";
+	}
+}
---
+++ CSE 360 Assignment 2/src/cse360assign2/CalculatorTest.java
@@ -0,0 +1,17 @@
+package cse360assign2;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		
+		c.add(5);
+		c.multiply(2);
+		c.subtract(2);
+		c.divide(2);
+		c.getHistory();
+		//System.out.println(c.getTotal());
+	}
+
+}
